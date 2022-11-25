e68ade792d478b0962df3b96113811df56940a3d
Ilhamjamal785 <muhammadgo144@gmail.com>
2022-11-25 22:07:21 +0700

    Initial commit

---
+++ app/src/main/java/com/ilham/intent/MoveActivity.kt
@@ -0,0 +1,13 @@
+package com.ilham.intent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MoveActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_move)
+
+
+    }
+}
