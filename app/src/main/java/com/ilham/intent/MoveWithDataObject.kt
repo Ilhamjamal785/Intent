7d00a429d2d016362ec08c9161af8997c9dd3efc
Ilhamjamal785 <muhammadgo144@gmail.com>
2023-01-30 12:51:32 +0700

    Modul Intent

---
+++ app/src/main/java/com/ilham/intent/MoveWithDataObject.kt
@@ -0,0 +1,11 @@
+package com.ilham.intent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MoveWithDataObject : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_move_with_data_object)
+    }
+}
